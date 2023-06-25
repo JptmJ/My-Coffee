b126aacb4da0aee0994c082d75791bd999672f5a
JptmJ <joshiptm797@gmail.com>
2023-06-25 22:46:27 +0530

    update
    update for further things

--- abc/dashboard/dbCon.php
+++ abc/dashboard/dbCon.php
@@ -1,23 +1,25 @@
 <!-- dbCon.php -->
-<?php 
-function connect($flag=TRUE){
+<?php
+function connect($flag = TRUE)
+{
+
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$dbName = "res_booking";
 
 	// Create connection
-	if($flag){
-		$conn = new mysqli($servername, $username, $password,$dbName);
-	}else{
+	if ($flag) {
+		$conn = new mysqli($servername, $username, $password, $dbName);
+	} else {
 		$conn = new mysqli($servername, $username, $password);
 	}
 	// Check connection
 	if ($conn->connect_error) {
 		die("Connection failed: $conn->connect_error");
-	} 
+	}
 	//echo "Connected successfully";
-	
+
 	return $conn;
 }
 
